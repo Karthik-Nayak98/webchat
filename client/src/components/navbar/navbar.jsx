f9c16faa813fecceb2010f585ec23070463b7688
karthik nayak <karunayak63@gmail.com>
2022-02-12 20:39:04 +0530

    feat: add create-room button.

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaDesktop } from "react-icons/fa";
 
-function NavBar() {
+function NavBar({ handleOpenModal }) {
   return (
-    <nav className='flex justify-between bg-blue-500 py-3 px-2 font-medium capitalize text-white'>
+    <nav className='flex justify-between bg-blue-600 py-3 px-2 font-medium capitalize text-white'>
       <span className='flex items-center justify-center gap-2 px-4 text-xl uppercase tracking-wide'>
         <FaDesktop />
         WebChat
       </span>
-      <Link
-        to='/'
-        className='rounded-md bg-gray-200 px-2 py-1 text-sm font-medium capitalize text-black'>
-        Leave Server
-      </Link>
+      <div className='flex gap-4'>
+        <button
+          onClick={handleOpenModal}
+          className='rounded-md bg-green-200 p-2 text-sm font-medium capitalize text-black'>
+          Create Room
+        </button>
+        <Link
+          to='/'
+          className='rounded-md bg-red-200 p-2 text-sm font-medium capitalize text-black'>
+          Leave Server
+        </Link>
+      </div>
     </nav>
   );
 }
